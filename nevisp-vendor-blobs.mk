6aca2235522e5a0c17da91c4434d2c60c38759a8
Bert Jansen <ajjansen.bj@gmail.com>
2015-11-21 05:53:28 +0100

    revert

--- nevisp-vendor-blobs.mk
+++ nevisp-vendor-blobs.mk
@@ -75,7 +75,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/nevisp/proprietary/lib/libisp2.so:system/lib/libisp2.so \
     vendor/samsung/nevisp/proprietary/lib/liblvvefs.so:system/lib/liblvvefs.so \
     vendor/samsung/nevisp/proprietary/lib/libomission_avoidance.so:system/lib/libomission_avoidance.so \
-    vendor/samsung/nevisp/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
     vendor/samsung/nevisp/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/samsung/nevisp/proprietary/lib/librpc.so:system/lib/librpc.so \
     vendor/samsung/nevisp/proprietary/lib/libsamsungRecord.so:system/lib/libsamsungRecord.so \
